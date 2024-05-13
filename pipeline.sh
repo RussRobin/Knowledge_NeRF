3f5f95a3466f00f5418bce232bb87c468bcb7e5e
Russell <82788441+RussRobin@users.noreply.github.com>
2024-05-13 10:50:31 +0800

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
-CONFIG1=/home/cwx/NeRF/nerf-pytorch/configs/car-in-1.txt
-CONFIG2=/home/cwx/NeRF/nerf-pytorch/configs/car-in-2.txt
+CONFIG1=car-in-1.txt
+CONFIG2=car-in-2.txt
 
 
-python run_nerf1.py --config $CONFIG1 --ft_path '/home/cwx/NeRF/nerf-pytorch/logs/car/200000.tar' --step1_initialize
-python run_nerf1.py --config $CONFIG2 --ft_path '/home/cwx/NeRF/nerf-pytorch/logs/car-in/250000.tar' --step2_freeze
-# python run_nerf1.py --config $CONFIG2 --ft_path '/home/cwx/NeRF/nerf-pytorch/logs/car-in/450000.tar' --step3_unfreeze
+python run_nerf.py --config $CONFIG1 --ft_path './logs/car/200000.tar' --step1_initialize
+python run_nerf.py --config $CONFIG2 --ft_path './logs/car-in/250000.tar' --step2_freeze
+python run_nerf.py --config $CONFIG2 --ft_path './logs/car-in/450000.tar' --step3_unfreeze
